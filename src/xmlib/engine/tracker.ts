0fc5ee7619595c3862c7bda18fed36f705f08064
Adam Vesecký <vesecky.adam@gmail.com>
2019-02-08 22:37:15 +0100

    Implement tracker

---
+++ src/xmlib/engine/tracker.ts
@@ -0,0 +1,114 @@
+import Effects from "./effects";
+import { XMFile } from './xmfile';
+import { XMContext, Channel } from './context';
+import { calcPeriod } from "./utils";
+import { SoundProcessor } from "./processor";
+import { Mixer } from './mixer';
+
+export default class Tracker {
+
+    xmFile: XMFile = null;
+    context: XMContext = null;
+    processor: SoundProcessor = new SoundProcessor();
+    mixer: Mixer = new Mixer();
+
+    // initialize all player variables to defaults prior to starting playback
+    initialize(xmFile: XMFile, sampleRate: number) {
+        this.xmFile = xmFile;
+
+        this.context = new XMContext();
+        this.context.sampleRate = sampleRate;
+
+
+        if (this.xmFile.initSpeed != 0) this.context.currentSpeed = this.xmFile.initSpeed;
+        if (this.xmFile.initBPM != 0) this.context.currentBpm = this.xmFile.initBPM;
+
+        // create channel table
+        for (let i = 0; i < this.xmFile.channelsNum; i++) {
+            this.context.channels[i] = new Channel();
+        }
+
+        this.processor.initialize(this.context, this.xmFile);
+        this.mixer.initialize(this.context, this.xmFile);
+    }
+
+    public repeat() {
+        this.context.position = 0;
+        this.context.endOfSong = false;
+    }
+
+    // mix a buffer of audio for an audio processing event
+    public mix(bufs: Float32Array[], buflen: number) {
+
+        // fill audiobuffer
+        for (let s = 0; s < buflen; s++) {
+
+            // if STT has run out, step player forward by tick
+            if (this.context.spd <= 0) {
+                this.advance();
+                this.processor.processTick();
+            }
+
+            this.mixer.mix(bufs, s);
+            this.context.spd--;
+        }
+    }
+
+    // advance player by a tick
+    // TODO rename to processPattern
+    private advance() {
+        // 125 BPM is the default MilkyTracker settings that gives 50 Hz
+        this.context.spd = Math.floor((125.0 / this.context.currentBpm) * (this.context.sampleRate / 50.0)); // 50Hz
+
+        // advance player
+        this.context.tick++;
+        this.context.flags |= 1;
+
+        // new row on this tick?
+        if (this.context.tick >= this.context.currentSpeed) {
+            if (this.context.patternDelay) { // delay pattern
+                if (this.context.tick < ((this.context.patternWait + 1) * this.context.currentSpeed)) {
+                    this.context.patternWait++;
+                } else {
+                    this.context.row++;
+                    this.context.tick = 0;
+                    this.context.flags |= 2;
+                    this.context.patternDelay = 0;
+                }
+            } else {
+                if (this.context.flags & 0x70) { // 0111 0000 -> if there is a global flag set
+                    if (this.context.flags & 0x40) { // 0100 0000 -> loop pattern?
+                        this.context.row = this.context.loopRow;
+                        this.context.flags &= 0xa1; // 1010 0001 -> 1, 16, 64 
+                        this.context.flags |= 2;
+                    } else if (this.context.flags & 16) { // pattern jump/break?
+                        this.context.position = this.context.patternJump;
+                        this.context.row = this.context.breakRow;
+                        this.context.patternJump = 0;
+                        this.context.breakRow = 0;
+                        this.context.flags &= 0xe1; // 1110 0001 -> 1, 16, 32, 64
+                        this.context.flags |= 2;
+                    }
+
+                    this.context.tick = 0;
+                } else {
+                    this.context.row++;
+                    this.context.tick = 0;
+                    this.context.flags |= 2;
+                }
+            }
+        }
+
+        // step to new pattern?
+        if (this.context.row >= this.xmFile.patternLength[this.xmFile.patternOrderTable[this.context.position]]) {
+            this.context.position++;
+            this.context.row = 0;
+            this.context.flags |= 4;
+        }
+
+        // end of song?
+        if (this.context.position >= this.xmFile.songLength) {
+            this.context.endOfSong = true;
+        }
+    }
+}
